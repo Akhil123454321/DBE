dfb7921d591eea680b43d59ae66e1817ae25dc12
Akhil123454321 <akhilskasturi@outlook.com>
2021-07-31 01:05:29 +0530

    just made some changes

--- app.js
+++ app.js
@@ -2,8 +2,9 @@
 const express = require('express')
 const path = require('path')
 const bodyparser = require('body-parser')
-// const mongoose = require('mongoose')
-const {request, response} = require("express");
+const mongoose = require('mongoose')
+const mailer = require('nodemailer')
+const bcrypt = require('bcrypt')
 
 //STARTING THE APP
 var app = express()
@@ -18,15 +19,46 @@ app.set('view engine','hbs')
 //BODY PARSER SETTINGS
 var urlencodedParser = bodyparser.urlencoded({ extended: false })
 
+app.use(session({
+    secret: 'secret',
+    cookie: {maxAge: 60000},
+    resave:false,
+    saveUninitialized: false
+}))
+app.use(flash())
 //DECLARING THE ROUTES
+//home page
 app.get('/', (request, response)=>{
+    response.render('home')
+})
+
+//login page
+app.get('/login', (request, response)=>{
+    response.render('login')
+})
+app.post('/login', urlencodedParser, (request, response)=>{
+    console.log(request.body);
+})
+
+//signup page
+app.get('/signup', (request, response)=>{
+    response.render('register')
+})
+app.post('/signup', urlencodedParser, (request, response)=>{
+    console.log(request.body);
+    console.log(request.body.pass)
+
+    
+})
+
+app.get('/trial', (request, response)=>{
     response.render('index')
 })
 
 //SETTING THE LISTENER PORT OF THE APPLICATION
 const PORT = process.env.PORT || 3000 //when the app is deployed onto a server, it will try to find the assigned port value. If there is no port value assigned in the server path variables, then the application runs on the default port 3000.
 app.listen(PORT, ()=>{
-    console.log("App is listening on port '"+PORT+"'")
+    console.log(`App is running on port ${PORT}`)
 })
 
 
